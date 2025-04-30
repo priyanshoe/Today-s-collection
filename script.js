a1e66c2d752675d5e8d7263e59f21a9dc4c4a662
priyanshoe <priyanshurahate@gmail.com>
2025-04-30 17:16:38 +0530

    Rebuild

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 var tl = gsap.timeline();
+var tl2 = gsap.timeline();
 
-tl.from('.heading h1',{
-    delay: 1,
-    duration: 1,
-    y: -30,
-    opacity: 0,
-    ease: 'Cubic.easeOut'
-})
-.from('.heading p',{
-    // duration: .5,
-    y:-15,
+tl.
+from('.head span',{
     opacity:0,
-    ease: 'Cubic.easeOut'
+    stagger:.1,
 })
 .from('.box',{
-    duration: .7,
-    x: "-100%",
-    height: 0,
-    opacity: 0,
-    ease: 'Expo.easeOut'
-})
-.from('.nav h2',{
+    height:0,
     opacity:0,
-    duration:1
+    stagger:.3,
+
 })
+.from('.box h2',{
+    y:50,
+    opacity:0,
+    stagger:.3,
+})
+.from('.box h4',{
+    y:50,
+    opacity:0,
+    stagger:.3,
+})
+
+tl2
+.from('.para span',{
+    delay:5,
+    opacity:0,
+    stagger:.1,
+    duration:2
+})
+.from('nav',{
+    x:-100,
+    opacity:0,
+    duration:1.5
+})
+
+
+let width = window.innerWidth;
+window.addEventListener('resize',() => {
+    
+    if(window.innerWidth !== width){
+        location.reload();
+    }
+});
